6739872343b0b571b211120a1e6ddeb919ad6e07
Peter Evans <peter-evans@users.noreply.github.com>
2019-10-31 13:59:44 +0900

    Fix sha input

--- dist/index.js
+++ dist/index.js
@@ -384,15 +384,18 @@ async function run() {
   try {
     const inputs = {
       token: core.getInput("token"),
+      sha: core.getInput("sha"),
       body: core.getInput("body"),
       path: core.getInput("path"),
       position: core.getInput("position")
     };
-
     core.debug(`Inputs: ${inspect(inputs)}`);
 
+    const sha = inputs.sha ? inputs.sha : process.env.GITHUB_SHA;
+    core.debug(`SHA: ${sha}`);
+
     await request(
-      `POST /repos/${process.env.GITHUB_REPOSITORY}/commits/${process.env.GITHUB_SHA}/comments`,
+      `POST /repos/${process.env.GITHUB_REPOSITORY}/commits/${sha}/comments`,
       {
         headers: {
           authorization: `token ${inputs.token}`
