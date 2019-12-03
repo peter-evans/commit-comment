c570795cf261a683ea20ad17a9787eb5a623ff37
Sachin Malhotra <sachinmalhotra@sachin.lan>
2019-12-02 20:31:46 -0500

    add eval support

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function run() {
         headers: {
           authorization: `token ${inputs.token}`
         },
-        body: `${inputs.body}`,
+        body: eval('`'+inputs.body+'`'),
         path: `${inputs.path}`,
         position: `${inputs.position}`
       }
