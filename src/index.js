4ab973b2528bd0d0da85ff79407dc978131cf14f
Peter Evans <18365890+peter-evans@users.noreply.github.com>
2021-05-12 10:44:52 +0900

    Add deprecation warning

--- src/index.js
+++ src/index.js
@@ -12,6 +12,11 @@ function getSha() {
 
 async function run() {
   try {
+    core.warning(`This action's default branch has been renamed to "main"`);
+    core.warning(
+      `Referencing this action with "@master" is deprecated and will stop working after June 30th 2021`
+    );
+
     const inputs = {
       token: core.getInput("token"),
       repository: core.getInput("repository"),
